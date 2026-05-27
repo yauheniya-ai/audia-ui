4e94eaba8bc7fba6cc76a6637aa822b1c1de5710
Yauheniya <149781090+yauheniya-ai@users.noreply.github.com>
2026-05-27 09:02:48 +0200

    fix: Update build path in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: "../pypi/src/audia/ui/static",
+    outDir: "../audia/src/audia/ui/static",
     emptyOutDir: true,
   },
   server: {
